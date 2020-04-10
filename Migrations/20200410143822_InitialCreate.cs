de73ea6d74f680a0f52ad26784d1e5233a1f67a8
Yernar Abilbay <eabilbay@gmail.com>
2020-04-10 23:38:26 +0600

    cxzcxz

---
+++ Migrations/20200410143822_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesStudent.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    birthDay = table.Column<DateTime>(nullable: false),
+                    groupNumber = table.Column<int>(nullable: false),
+                    code = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RazorPagesStudent.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+
+
+        [Display(Name = "Birthday")]
+        public DateTime birthDay { get; set; }
+
+
+        [Display(Name = "Group number")]
+        public int groupNumber { get; set; }
+
+        public string code { get; set; }
+
+    }
+}
